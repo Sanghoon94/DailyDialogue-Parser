88a83c2e7a060168546b5d1b985d774c23379f3c
peter szemraj <peter.szemraj@gmail.com>
2022-08-26 07:25:04 +0200

    :recycle: :art: clean up & streamline

--- parser.py
+++ parser.py
@@ -4,49 +4,49 @@
 
 __author__ = "Sanghoon Kang"
 
-import os, sys, getopt, gzip
+import argparse
+import gzip
+import logging
+import os
+import sys
+from pathlib import Path
+
+from tqdm import tqdm
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def parse_data(in_dir, out_dir):
 
     # Finding files
-    if in_dir.endswith("train"):
-        dial_dir = os.path.join(in_dir, "dialogues_train.txt")
-        emo_dir = os.path.join(in_dir, "dialogues_emotion_train.txt")
-        act_dir = os.path.join(in_dir, "dialogues_act_train.txt")
-    elif in_dir.endswith("validation"):
-        dial_dir = os.path.join(in_dir, "dialogues_validation.txt")
-        emo_dir = os.path.join(in_dir, "dialogues_emotion_validation.txt")
-        act_dir = os.path.join(in_dir, "dialogues_act_validation.txt")
-    elif in_dir.endswith("test"):
-        dial_dir = os.path.join(in_dir, "dialogues_test.txt")
-        emo_dir = os.path.join(in_dir, "dialogues_emotion_test.txt")
-        act_dir = os.path.join(in_dir, "dialogues_act_test.txt")
-    else:
-        print("Cannot find directory")
-        sys.exit()
-
-    out_dial_dir = os.path.join(out_dir, "dial.txt.gz")
-    out_emo_dir = os.path.join(out_dir, "emo.txt.gz")
-    out_act_dir = os.path.join(out_dir, "act.txt.gz")
+    dirname = in_dir.name
+    dial_dir = in_dir / f"dialogues_{dirname}.txt"
+    emo_dir = in_dir / f"dialogues_emotion_{dirname}.txt"
+    act_dir = in_dir / f"dialogues_act_{dirname}.txt"
+    assert dial_dir.exists() and emo_dir.exists() and act_dir.exists(), f"{in_dir} does not contain the required files"
+    out_dial_path = out_dir / "dial.txt.gz"
+    out_emo_path = out_dir / "emo.txt.gz"
+    out_act_path = out_dir / "act.txt.gz"
 
     # Open files
     in_dial = open(dial_dir, "r", encoding="utf-8", errors="ignore")
     in_emo = open(emo_dir, "r", encoding="utf-8", errors="ignore")
     in_act = open(act_dir, "r", encoding="utf-8", errors="ignore")
+
     out_dial = gzip.open(
-        out_dial_dir,
+        out_dial_path,
         "w",
     )
     out_emo = gzip.open(
-        out_emo_dir,
+        out_emo_path,
         "w",
     )
     out_act = gzip.open(
-        out_act_dir,
+        out_act_path,
         "w",
     )
-
+    pbar = tqdm(desc="Parsing", )
     for line_count, (line_dial, line_emo, line_act) in enumerate(
         zip(in_dial, in_emo, in_act)
     ):
@@ -65,8 +65,8 @@ def parse_data(in_dir, out_dir):
         act_len = len(acts)
 
         if seq_len != emo_len or seq_len != act_len:
-            print(
-                "Different turns btw dialogue & emotion & acttion! ",
+            logging.warning(
+                "Different turns btw dialogue & emotion & action! ",
                 line_count + 1,
                 seq_len,
                 emo_len,
@@ -98,6 +98,8 @@ def parse_data(in_dir, out_dir):
                 out_act.write("\n".encode("utf-8"))
 
             seq_count += 1
+        pbar.update(1)
+    pbar.close()
 
     in_dial.close()
     in_emo.close()
@@ -106,31 +108,48 @@ def parse_data(in_dir, out_dir):
     out_emo.close()
     out_act.close()
 
+def get_parser():
+    """
+    get_parser - a helper function for the argparse module
+    """
 
-def main(argv):
+    parser = argparse.ArgumentParser(
+        description="parser.py - a parser for the daily dialog dataset"
+    )
+    parser.add_argument(
+        "-i",
+        "--in_dir",
+        type=Path,
+        help="Input directory containing the dialogues",
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--out_dir",
+        type=Path,
+        help="Output directory for the parsed dialogues",
+        required=True,
+    )
+    return parser
 
-    in_dir = ""
-    out_dir = ""
-
-    try:
-        opts, args = getopt.getopt(argv, "h:i:o:", ["in_dir=", "out_dir="])
-    except getopt.GetoptError:
-        print("python3 parser.py -i <in_dir> -o <out_dir>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == "-h":
-            print("python3 parser.py -i <in_dir> -o <out_dir>")
-            sys.exit()
-        elif opt in ("-i", "--in_dir"):
-            in_dir = arg
-        elif opt in ("-o", "--out_dir"):
-            out_dir = arg
-
-    print("Input directory : ", in_dir)
-    print("Ouptut directory: ", out_dir)
-
-    parse_data(in_dir, out_dir)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+
+    args = get_parser().parse_args()
+    logging.info(f"args:\n{args}")
+    in_dir = Path(args.in_dir)
+    out_dir = Path(args.out_dir)
+
+    # check if paths are full paths, if not, assume they are relative to the current working directory
+    if not in_dir.is_absolute():
+        logging.info("assuming input directory is relative to the current working directory")
+        in_dir = Path.cwd() / in_dir
+        out_dir = Path.cwd() / out_dir
+
+    logging.info(f"Input directory:\n\t{in_dir}")
+    logging.info(f"Output directory:\n\t{out_dir}")
+
+    parse_data(in_dir, out_dir)
+
+    print("Done")
